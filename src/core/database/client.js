c649c57a3bf5e833c3c5e62789fdd2f41e2197eb
Rethers mathieu <71334121+RethersMathieu@users.noreply.github.com>
2022-04-27 20:33:16 +0200

    console log

--- src/core/database/client.js
+++ src/core/database/client.js
@@ -3,6 +3,7 @@ const { MongoClient } = require('mongodb');
 const URI = 'mongodb+srv://ESP_NodeJS_IoT:Rm5YjSvPzMJz32gV@iot-tp.7vrbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 async function initClient() {
+    console.log('CREATE CONNECTOR');
     try {
         return await MongoClient.connect(URI);
     } catch (e) {
