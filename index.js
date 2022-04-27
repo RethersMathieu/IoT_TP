a61044d4b4b409c73a249e4bbbf60e05fe792dd7
Rethers mathieu <71334121+RethersMathieu@users.noreply.github.com>
2022-04-27 21:31:17 +0200

    test

--- index.js
+++ index.js
@@ -79,4 +79,11 @@ async function init() {
     });
 }
 
-init().catch(console.error);
+const express = require ('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.send('HELLO;WORLD!');
+});
+app.listen(3000, function () {
+    console.log('INIT');
+});
