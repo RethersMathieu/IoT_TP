a4bf801e3f694c5f216347eeca41c99470176d77
Alcaraz Yannick <yannick.alcaraz@etu.unice.fr>
2022-04-28 22:14:49 +0200

    Debug css

--- src/controllers/pages.js
+++ src/controllers/pages.js
@@ -5,5 +5,7 @@ function getUriPage() {
 }
 
 module.exports = {
-  '': function (req, res) { res.sendFile(path.join(`${getUriPage()}/accueil/ui_lucioles.html`)); },
+  '/': function (req, res) { res.sendFile(path.join(`${getUriPage()}/accueil/ui_lucioles.html`)); },
+  '/login': function (req, res) { res.sendFile(path.join(`${getUriPage()}/logIn/logIn.html`)); },
+  '/signUp': function (req, res) { res.sendFile(path.join(`${getUriPage()}/logIn/signUp.html`)); },
 };
