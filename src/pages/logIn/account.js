404533d29eff05c629fc19b31e32b9656f9a2d9d
Rethers mathieu <71334121+RethersMathieu@users.noreply.github.com>
2022-05-01 20:41:40 +0200

    Affichage erreur

--- src/pages/logIn/account.js
+++ src/pages/logIn/account.js
@@ -1,11 +1,24 @@
+window.onload = function () {
+    const page = location.pathname.split('/')[1];
+    if (page === 'login') initLogin();
+    else if (page === 'signup') initSignup();
+}
+
 function initLogin() {
     const form = $('form');
     form.on('submit', function (event) {
         event.preventDefault();
-        send('/connexion/login', $(this).serializeArray(), function (session) {
-            sessionStorage.setItem('user', JSON.stringify(session));
-            location.href = location.origin.concat('/graphs');
-        });
+        send(
+            '/connexion/login',
+            $(this).serializeArray(),
+            function (session) {
+                sessionStorage.setItem('user', JSON.stringify(session));
+                location.href = location.origin.concat('/graphs');
+            },
+            function () {
+                printMessageError('Votre nom ou votre adresse mac ne sont pas enregistré.');
+            }
+        );
     });
 }
 
@@ -13,9 +26,16 @@ function initSignup() {
     const form = $('form');
     form.on('submit', function (event) {
         event.preventDefault();
-        send('/connexion/signup', $(this).serializeArray(), function (res) {
-            location.href = location.origin.concat('/login');
-        });
+        send(
+            '/connexion/signup',
+            $(this).serializeArray(),
+            function (res) {
+                location.href = location.origin.concat('/login');
+            },
+            function () {
+                printMessageError('L\'utilisateur est déjà exitant.');
+            }
+        );
     })
 }
 
@@ -31,8 +51,14 @@ function send(path, data, succes, error = (r, status, err) => console.error(r)) 
     });
 }
 
-window.onload = function () {
-    const page = location.pathname.split('/')[1];
-    if (page === 'login') initLogin();
-    else if (page === 'signup') initSignup();
+function printMessageError(msg) {
+    const form = document.getElementById('contenu');
+    if (form) {
+        const msgErr = document.createElement('p');
+        msgErr.style.color = '#D31805';
+        msgErr.style.fontWeight = 'bold';
+        msgErr.textContent = msg;
+
+        form.appendChild(msgErr);
+    }
 }
