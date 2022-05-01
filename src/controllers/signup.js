5676f5063911030f479cfd01cf6c8e9f5ab0a335
Rethers mathieu <71334121+RethersMathieu@users.noreply.github.com>
2022-05-01 12:19:32 +0200

    correction signup

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,12 +1,16 @@
 function signup(dbo, callback) {
     return function ({ body }, res, next) {
-        const newUser = { name: body.name, mac: body.mac };
+        const newUser = { name: body.name.toLowerCase(), mac: body.mac.toUpperCase() };
         dbo.collection('user').findOne(newUser, function (err, user) {
             if (err) return res.status(500).json({ error: 'Erreur inatandue.' });
-            if (user) return res.status(401).json({ error: 'L\'utilisateur est déjà exitant.' });
-            dbo.collection('in-waiting').insertOne(newUser, function (_err, reponse) {
-                if (_err || !reponse) return res.status(500).json({ error: 'Erreur inatandue.' })
-                callback(newUser, { res, next });
+            else if (user) return res.status(401).json({ error: 'L\'utilisateur est déjà exitant.' });
+            dbo.collection('in_waiting').findOne(newUser, function (err, user) {
+                if (err) return res.status(500).json({ error: 'Erreur inatandue.' });
+                else if (user) return res.status(401).json({ error: 'L\'utilisateur est déjà exitant.' });
+                dbo.collection('in_waiting').insertOne(newUser, function (_err, reponse) {
+                    if (_err || !reponse) return res.status(500).json({ error: 'Erreur inatandue.' })
+                    callback(newUser, { res, next });
+                });
             });
         });
     }
